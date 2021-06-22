7a1aabb2296bac343ebcf66077e5e5f487b1d026
Ben Ethington <benaminde@gmail.com>
2021-06-22 15:09:23 -0500

    Add typecasting and cleaned up code

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
 import express from 'express';
-const path = require('path');
-const router = express.Router();
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
-import { fileURLToPath } from 'url';
 
 (async () => {
 
@@ -15,12 +12,16 @@ import { fileURLToPath } from 'url';
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req:express.Request, res:express.Response ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -28,20 +29,9 @@ import { fileURLToPath } from 'url';
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  //    the filtered image file
+  app.get( "/filteredimage", async ( req:express.Request, res:express.Response ) => {
 
-  /**************************************************************************** */
-
-  //! END @TODO1
-  
-  // Root Endpoint
-  // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-
-  app.get( "/filteredimage", async ( req, res ) => {
-    
     let image_url = req.query.image_url as string;
 
     if (req.query.image_url) {
